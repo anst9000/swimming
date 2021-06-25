4f551fdf4077328a938acf8848fcd52f8f6e9555
Anders Strömberg <coltlaboy@yahoo.se>
2021-06-25 21:13:03 +0200

    Fixade så all ät centrerat

--- main.js
+++ main.js
@@ -34,19 +34,3 @@ function showListBad() {
     badButton.innerText = "Visa Mer";
   }
 }
-var caller = $("#emcee"),
-  box = $("#box");
-
-function foo() {
-  var randX = Math.floor(Math.random() * (window.innerWidth - 100));
-  var randY = Math.floor(Math.random() * (window.innerHeight - 100));
-  console.log([randX, randY]);
-  caller.stop().animate({ left: randX + "px", top: randY + "px" });
-}
-
-$(document).ready(function () {
-  caller.on("mouseenter", foo);
-  caller.on("click", function () {
-    alert("clicked!");
-  });
-});
